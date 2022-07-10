b59a594435ca3d2d7add815d81a174649450af26
Felipe Mendes <felipemendesdev@gmail.com>
2022-07-10 18:36:02 -0300

    Text formatting

---
+++ tabuada.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""Imprime a tabuada do 1 ao 10"""
+__version__ = "0.1.0"
+__author__ = "Felipe Mendes"
+
+numeros = list(range(1,11))
+
+for n1 in numeros:
+    print("{:-^18}".format(f"---Tabuada do {n1}---"))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado} \n"))
+    print("#" * 18)
